58e409b8d446f2a0a38600543f3feb4df392b7b8
minwoo <mwb970419@gmail.com>
2025-04-29 15:41:40 +0900

    test: docker check

--- backend/baperang/src/main/java/com/ssafy/baperang/domain/user/repository/UserRepository.java
+++ backend/baperang/src/main/java/com/ssafy/baperang/domain/user/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.ssafy.baperang.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     // 로그인 아이디 중복만 체크
