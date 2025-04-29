a209eed30ae6c9ebcd5f11db2f768ed8a598acd9
이건욱 <minyminy18@naver.com>
2025-04-29 10:42:16 +0900

    feat : signup

--- backend/baperang/src/main/java/com/ssafy/baperang/domain/user/repository/UserRepository.java
+++ backend/baperang/src/main/java/com/ssafy/baperang/domain/user/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.ssafy.baperang.domain.user.repository;
 
-public interface UserRepository {
+import com.ssafy.baperang.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    // 로그인 아이디 중복만 체크
+    boolean existsByLoginId(String loginId);
 }
--- backend/baperang/src/main/java/com/ssafy/baperang/domain/user/service/UserService.java
+++ backend/baperang/src/main/java/com/ssafy/baperang/domain/user/service/UserService.java
@@ -1,4 +1,49 @@
 package com.ssafy.baperang.domain.user.service;
 
+import com.ssafy.baperang.domain.user.dto.request.SignupRequestDto;
+import com.ssafy.baperang.domain.user.dto.response.SignupResponseDto;
+import com.ssafy.baperang.domain.user.entity.User;
+import com.ssafy.baperang.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@RequiredArgsConstructor // final로 선언된 필드를 매개변수로 받는 생성자를 자동으로 만듬
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional // 모든 데이터베이스 작업을 하나의 트랜잭션으로 묶음
+    public SignupResponseDto signup(SignupRequestDto requestDto) {
+        // SignupRequestDto - 클라이언트에서 받은 데이터 담음
+        // SignupResponseDto - 클라이언트에게 돌려줄 응답
+        if (userRepository.existsByLoginId(requestDto.getLoginId())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "이미 존재하는 이디입니다.");
+        }
+
+        User user = User.builder()
+                .loginId(requestDto.getLoginId())
+                .password(requestDto.getPassword())
+                .nutritionistName(requestDto.getNutritionistName())
+                .city(requestDto.getCity())
+                .schoolName(requestDto.getSchoolName())
+                .build();
+
+        userRepository.save(user); // 만든 User 객체를 DB에 저장
+
+        SignupResponseDto.SignupContent content = SignupResponseDto.SignupContent.builder()
+                .loginId(user.getLoginId())
+                .nutritionistName(user.getNutritionistName())
+                .city(user.getCity())
+                .schoolName(user.getSchoolName())
+                .build();
+
+        return SignupResponseDto.builder()
+                .message("회원가입이 완료 되었습니다.")
+                .content(content)
+                .build();
+    }
 }
