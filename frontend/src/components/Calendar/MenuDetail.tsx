07b34bcace3cd609a6a9f560ab35ca8cba4b98df
김도연 <kdyveliz@naver.com>
2025-05-02 14:30:35 +0900

    Refactor.css 수수수수수수수수정정정

--- frontend/src/components/Calendar/MenuDetail.tsx
+++ frontend/src/components/Calendar/MenuDetail.tsx
@@ -10,14 +10,14 @@ const MenuDetail = ({ selectedDate, menuData }: MenuDetailProps) => {
     <div className="w-1/5 bg-[#FCF8F3]/90 rounded-3xl p-4 flex flex-col">
       {selectedDate && menuData[selectedDate] ? (
         <div className="flex flex-col h-full">
-          <div className="text-lg font-semibold text-center mb-3">
+          <div className="text-base font-semibold text-center mb-3">
             {menuData[selectedDate].date} 식단
           </div>
           <div className="flex-grow flex flex-col gap-3 items-center">
             {menuData[selectedDate].menu.map((item: string, index: number) => (
               <div
                 key={index}
-                className="bg-white w-full px-4 py-3 rounded-3xl text-base font-medium shadow-sm text-center"
+                className="bg-white w-full px-4 py-3 rounded-3xl text-xs shadow-sm text-center"
               >
                 {item}
               </div>
