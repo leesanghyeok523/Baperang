6504f500573e5710e222fffbd8ed6f9eca618268
김도연 <kdyveliz@naver.com>
2025-05-02 14:55:13 +0900

    Refactor.오류수정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,4 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
-// 개발 모드에서는 80%로, 배포 모드에서는 100%로 폰트 크기(=전체 배율)를 설정
-if (import.meta.env.DEV) {
-  document.documentElement.style.fontSize = '80%';
-} else {
-  document.documentElement.style.fontSize = '0%';
-}
-
 createRoot(document.getElementById('root')!).render(<App />);
