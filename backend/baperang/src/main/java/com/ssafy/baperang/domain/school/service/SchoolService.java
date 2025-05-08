ac4e8fd810a14eaa21f2d2ba751870a73354fb69
minwoo <mwb970419@gmail.com>
2025-05-08 17:06:27 +0900

    Fix: school 조회 수정

--- backend/baperang/src/main/java/com/ssafy/baperang/domain/school/service/SchoolService.java
+++ backend/baperang/src/main/java/com/ssafy/baperang/domain/school/service/SchoolService.java
@@ -1,11 +1,9 @@
 package com.ssafy.baperang.domain.school.service;
 
-import com.ssafy.baperang.domain.school.dto.request.SchoolsRequestDto;
-
 import com.ssafy.baperang.domain.school.dto.response.SchoolsResponseDto;
 import com.ssafy.baperang.domain.school.dto.response.CitiesResponseDto;
 
 public interface SchoolService {
     CitiesResponseDto getCities();
-    SchoolsResponseDto getSchools(SchoolsRequestDto schoolsRequestDto);
+    SchoolsResponseDto getSchools(String city, String schoolName);
 }
