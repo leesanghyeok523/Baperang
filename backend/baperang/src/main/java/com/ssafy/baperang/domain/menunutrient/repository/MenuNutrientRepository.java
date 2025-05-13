b1c25f795073fcb5d3d1c38e6ebf3295e28b6795
minwoo <mwb970419@gmail.com>
2025-05-13 12:14:15 +0900

    Feat : BE - 메뉴별 영양소 정보 조회

--- backend/baperang/src/main/java/com/ssafy/baperang/domain/menunutrient/repository/MenuNutrientRepository.java
+++ backend/baperang/src/main/java/com/ssafy/baperang/domain/menunutrient/repository/MenuNutrientRepository.java
@@ -1,4 +1,27 @@
 package com.ssafy.baperang.domain.menunutrient.repository;
 
-public interface MenuNutrientRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.baperang.domain.menu.entity.Menu;
+import com.ssafy.baperang.domain.menunutrient.entity.MenuNutrient;
+
+@Repository
+public interface MenuNutrientRepository extends JpaRepository<MenuNutrient, MenuNutrient.MenuNutrientId> {
+    
+    /**
+     * 메뉴 ID로 영양소 정보 조회
+     * @param menuId 메뉴 ID
+     * @return 해당 메뉴의 영양소 정보 목록
+     */
+    List<MenuNutrient> findByMenuId(Long menuId);
+    
+    /**
+     * Menu 엔티티로 영양소 정보 조회
+     * @param menu 메뉴 엔티티
+     * @return 해당 메뉴의 영양소 정보 목록
+     */
+    List<MenuNutrient> findByMenu(Menu menu);
 }
